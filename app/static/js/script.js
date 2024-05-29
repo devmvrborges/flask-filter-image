40c156a09b44b3e22088ac558ad29e15918a41bd
Mauricio Borges <mvrborges0@gmail.com>
2024-05-28 22:43:51 -0300

    chore: Update CSS and JS files
    This commit updates the CSS and JS files with the following changes:
    - Added a new CSS class `.color-container` for displaying colors
    - Added a new CSS class `.circle` for styling circles
    - Updated the `script.js` file to include a function `changeColor` for changing the background color of circles based on user selection
    - Updated the `script.js` file to include event listeners for the `resamplingMethod` and `gridSize` select elements, triggering the `updateStep1` function when the values are changed.
    
    These changes improve the styling and functionality of the application.

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -5,12 +5,17 @@ window.addEventListener('DOMContentLoaded', function () {
     var button = document.getElementById('upload');
     var cropper;
 
-   
+
     input.addEventListener('change', function () {
         var file = this.files[0];
         var url = URL.createObjectURL(file);
 
         image.src = url;
+
+        if (cropper) {
+            cropper.destroy();
+        }
+
         cropper = new Cropper(image, {
             aspectRatio: 1,
             viewMode: 1,
@@ -33,32 +38,86 @@ window.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    button.addEventListener('click', function () {
+    //     button.addEventListener('click', function () {
+    //         if (cropper) {
+    //             cropper.getCroppedCanvas().toBlob(function (blob) {
+    //                 var formData = new FormData();
+    //                 var gridSize = document.getElementById('gridSize').value; 
+    //                 var resamplingMethod = document.getElementById('resamplingMethod').value;
+
+    //                 formData.append('file', blob, 'image.jpg');
+    //                 formData.append('gridSize', gridSize);
+    //                 formData.append('resamplingMethod', resamplingMethod);
+
+    //                 fetch('/apply_filter', {
+    //                     method: 'POST',
+    //                     body: formData
+    //                 })
+    //                 .then(response => response.blob())
+    //                 .then(blob => {
+    //                     var url = URL.createObjectURL(blob);
+    //                     document.getElementById('cropped-filtred').src = url;
+    //                 })
+    //                 .catch(error => {
+    //                     console.error('Error:', error);
+    //                     alert('Image processing failed');
+    //                 });
+    //             });
+    //         }
+    //     });
+
+
+
+    var resamplingMethodSelect = document.getElementById('resamplingMethod');
+    var gridSizeSelect = document.getElementById('gridSize');
+
+
+    function updateStep1() {
         if (cropper) {
             cropper.getCroppedCanvas().toBlob(function (blob) {
                 var formData = new FormData();
-                var gridSize = document.getElementById('gridSize').value; 
+                var gridSize = document.getElementById('gridSize').value;
                 var resamplingMethod = document.getElementById('resamplingMethod').value;
 
-                
                 formData.append('file', blob, 'image.jpg');
                 formData.append('gridSize', gridSize);
                 formData.append('resamplingMethod', resamplingMethod);
-                
+
                 fetch('/apply_filter', {
                     method: 'POST',
                     body: formData
                 })
-                .then(response => response.blob())
-                .then(blob => {
-                    var url = URL.createObjectURL(blob);
-                    document.getElementById('cropped-filtred').src = url;
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                    alert('Image processing failed');
-                });
+                    .then(response => response.blob())
+                    .then(blob => {
+                        var url = URL.createObjectURL(blob);
+                        document.getElementById('cropped-filtred').src = url;
+                    })
+                    .catch(error => {
+                        console.error('Error:', error);
+                        alert('Image processing failed');
+                    });
             });
         }
+    }
+
+    resamplingMethodSelect.addEventListener('change', function () {
+        updateStep1();
     });
+    gridSizeSelect.addEventListener('change', function () {
+        updateStep1();
+    });
+}); 
+
+
+
+function changeColor(id) {
+    var select = document.getElementById(id);
+    var circle = document.getElementById("circle" + id.slice(-1));
+    console.log(select);
+    circle.style.backgroundColor = select.value;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
 });
